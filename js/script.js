5a9443370af39cd271c8c0598bee86d4ccd0609f
Roydano <roy.dano90@gmail.com>
2021-06-01 16:09:46 +0200

    add jsnack 2, 3

--- js/script.js
+++ js/script.js
@@ -1,13 +1,11 @@
-// Riscrivete gli snack fatti in classe apportando le correzioni viste assieme stamattina, più quelli nuovi.
+
+// L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga. ------ok
 
 
-// L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
+// Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri   inseriti. Esegui questo programma in due versioni, con il for e con il while. ------ok
 
 
-// Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri   inseriti. Esegui questo programma in due versioni, con il for e con il while.
-
-
-// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
+// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array ------ok
 
 
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
@@ -27,3 +25,53 @@ if (firstWord.length < secondWord.length){
 }
 
 document.getElementById('word').innerHTML = message;
+
+
+
+
+// JSNACK 2
+
+// METODO CON CICLO FOR
+
+var userNumber;
+var somma = 0;
+
+for (var i = 0; i < 5; i++){
+    userNumber = parseInt (prompt('Inserisci un numero'));
+    somma += userNumber;
+}
+
+document.getElementById('somma-for').innerHTML = 'La somma dei tuoi numeri con il ciclo For è: ' + somma;
+
+
+// METODO CON CICLO WHILE
+
+var userNumber;
+var somma = 0;
+var i = 0;
+
+while(i < 5){
+    i++;
+    userNumber = parseInt (prompt('Inserisci un numero'));
+    somma += userNumber;
+}
+
+document.getElementById('somma-while').innerHTML = 'La somma dei tuoi numeri con il ciclo While è: ' + somma;
+
+
+//JSNACK 3
+
+var numeri = [];
+
+for (var i = 0; i < 6; i++){
+    userNumber = parseInt (prompt('Inserisci un numero'));
+    if(userNumber % 2 != 0){
+        numeri.push(userNumber)
+    }
+}
+
+document.getElementById('dispari').innerHTML = 'I numeri dispari che hai inserito sono: ' + numeri;
+
+
+//JSNACK 4
+
